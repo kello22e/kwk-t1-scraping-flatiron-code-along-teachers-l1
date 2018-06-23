54eddf76084605d3fac5af00e1718fa8f4dd0cf5
kello22e <kello22e@mtholyoke.edu>
2018-06-23 21:04:55 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,13 @@ require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def get_page
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      binding.pry
+  end
+  
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +21,8 @@ class Scraper
       end
     end
   end
-  
-end
+
+Scraper.new.get_page 
 
 
 
